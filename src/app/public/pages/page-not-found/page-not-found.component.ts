e159893a41a75a6d6808c65ad56fcd5eae08ab5c
rivacortezf <u202215313@upc.edu.pe>
2024-04-22 20:55:59 -0500

    chore(server): added json-server configuration files.

--- src/app/public/pages/page-not-found/page-not-found.component.ts
+++ src/app/public/pages/page-not-found/page-not-found.component.ts
@@ -1,11 +1,12 @@
-import {ActivatedRoute, Router} from "@angular/router";
 import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
   styleUrl: './page-not-found.component.css'
 })
-export class PageNotFoundComponent implements OnInit{
+export class PageNotFoundComponent implements OnInit {
   invalidUrl: string;
 
   constructor(private route: ActivatedRoute, private router: Router) {
@@ -21,5 +22,6 @@ export class PageNotFoundComponent implements OnInit{
   ngOnInit(): void {
     this.invalidUrl = this.route.snapshot.url[0].path;
   }
-
 }
+
+
